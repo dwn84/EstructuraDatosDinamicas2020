a213db51e338bf1b22596665df0c929547313783
dwn84 <dwn84@hotmail.com>
2020-09-01 09:48:02 -0500

    Actualización lista simple

--- Lists/src/lists/Lists.java
+++ Lists/src/lists/Lists.java
@@ -16,6 +16,7 @@ public class Lists {
      */
     public static void main(String[] args) {
 
+
 //        // TODO code application logic here
 //        Node<Integer> vagon1 = new Node<>(11);
 //        Node<Integer> vagon2 = new Node<>(12);
@@ -29,19 +30,46 @@ public class Lists {
 //        vagon4.setNextNode(vagon5);
     
 //        System.out.println(vagon1.getNextNode().getNextNode().getNextNode().getData());       
+
+//        String a = "z", b="z";
+//        System.out.println("Resultado de la comparación: "+ a.compareTo(b));
+//        System.out.println("");
+//  
         SingleLinkedList<Integer> miListaS = new SingleLinkedList<>();
-        miListaS.add(11);
-        miListaS.add(12);
-        miListaS.add(13);
-        miListaS.add(14);
-        miListaS.add(15);
-        miListaS.add(16);
-        miListaS.add(32568);
-        miListaS.add(77);
-        miListaS.add(99);
+        miListaS.addLast(11);
+        miListaS.addLast(33);
+        miListaS.addLast(22);
+        miListaS.addLast(44);
+        miListaS.addLast(55);
+
+//          miListaS.addOrdered(99);
+//          miListaS.addOrdered(10);
+//          miListaS.addOrdered(33);
+//          miListaS.addOrdered(22);
+//          miListaS.addOrdered(55);
+//          miListaS.addOrdered(66);
+//          miListaS.addOrdered(8);
+          
+//        miListaS.add(11);
+//        miListaS.add(12);
+//        miListaS.add(13);
+//        miListaS.add(14);
+//        miListaS.add(15);
+//        miListaS.add(16);
+//        miListaS.add(32568);
+//        miListaS.add(77);
+//        miListaS.add(99);
         System.out.println(miListaS.showData());
+        miListaS.delete();
+        miListaS.delete();
+
+        System.out.println(miListaS.showData());
+
+        
     
     }
+    
+
 
   
 }
--- Lists/src/lists/SingleLinkedList.java
+++ Lists/src/lists/SingleLinkedList.java
@@ -9,7 +9,7 @@ package lists;
  *
  * @author samaniw
  */
-public class SingleLinkedList<T> implements Ilist<T> {
+public class SingleLinkedList<T extends Number & Comparable> implements Ilist<T> {
 
     private Node<T> head;
     private int size;
@@ -45,14 +45,68 @@ public class SingleLinkedList<T> implements Ilist<T> {
 
     @Override
     public void addLast(T d) {
+        if(isEmpty()){
+            add(d);
+        }else{
+            Node<T> current=head;
+            while(current.getNextNode()!=null){
+                current = current.getNextNode();
+            }
+            Node<T> newNode = new Node<>(d);
+            current.setNextNode(newNode);
+        }
+        
+
+        
     }
 
     @Override
     public void addOrdered(T d) {
+        /*Si la lista esta vacía, 
+        o el valor del primer elemento de la lista(cabeza) 
+        es mayor que el nuevo, insertar el nuevo nodo en la 
+        primera posición de la lista y modificar la cabecera 
+        respectivamente.
+        */
+        if(isEmpty() || head.getData().compareTo(d)>0){
+            add(d);
+        }else{
+        /*
+        Si no se cumple el caso anterior, buscar el lugar adecuado
+        para la inserción: recorrer la lista conservando el nodo
+        actual. Inicializar nodo actual con el valor de primera
+        posición, y avanzar mientras el siguiente nodo no sea nulo 
+        y el dato que contiene la siguiente posición sea menor 
+        o igual que el dato a insertar.
+        */
+        
+        Node<T> current = head;
+        while(current.getNextNode()!=null
+              && current.getNextNode().getData().compareTo(d)<0
+                ){
+            current = current.getNextNode();
+        }        
+        /*Con esto el nodo actual señala al nodo adecuado,
+        a continuación insertar el nuevo nodo después de él.
+        */
+        Node<T> newNode = new Node<>(d);
+        newNode.setNextNode(current.getNextNode());
+        current.setNextNode(newNode);        
+        }
+        
+        size++;
     }
 
     @Override
     public void delete() {
+        if(isEmpty()){
+            System.out.println("Lista vacia");
+        }else{
+            //apuntar la cabeza al segundo dato
+            head = head.getNextNode();
+        }
+        size--;
+        
     }
 
     @Override
@@ -65,8 +119,15 @@ public class SingleLinkedList<T> implements Ilist<T> {
             return "Lista vacia";
         }else{
             String data = "";
+            //Forma tradicional de recorrer la estructura
+//            Node<T> current=head;
+//            while(current!=null){
+//                data += current.getData() + " ";
+//                current = current.getNextNode();
+//            }
+            
             //for (int i = 0; i < n; i=i+1) {
-            for(Node i=head;i!=null;i = i.getNextNode()){
+            for(Node<T> i=head;i!=null;i = i.getNextNode()){
                 data += i.getData() + " ";
             } 
             return data;
