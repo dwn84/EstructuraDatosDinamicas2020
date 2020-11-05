b751a3d4e14f507fa2d29274243fe2827b76f441
dwn84 <dwn84@hotmail.com>
2020-11-05 09:28:05 -0500

    Agregar datos al árbol de búsqueda binario

--- Trees/src/trees/BinarySearchTree.java
+++ Trees/src/trees/BinarySearchTree.java
@@ -10,21 +10,91 @@ package trees;
  * @author samaniw
  */
 public class BinarySearchTree {
-    
+
     public BinaryNode root;
     private BinaryNode father;
     private boolean fatherposition;
     private int nodes;
     private int leafs;
-    
+
     public BinarySearchTree() {
         root = null;
         nodes = 0;
     }
-    
+
     public BinarySearchTree(int data) {
         root = new BinaryNode(data);
         nodes = 1;
     }
-    
+
+    public BinaryNode search(int data) {
+        return search(data, root);
+    }
+
+    private BinaryNode search(int data, BinaryNode currentNode) {
+        if (currentNode == null) {
+            return null;
+        } else if (data == currentNode.getData()) {
+            return currentNode;
+        } else if (data > currentNode.getData()) {
+            return search(data, currentNode.getRight());
+        } else {
+            return search(data, currentNode.getLeft());
+
+        }
+    }
+
+    public void add(int data) {
+        if (root == null) {
+            root = new BinaryNode(data);
+        } else if (search(data) != null) {
+            System.out.println("No duplicate vertex allowed!");
+        } else {
+            add(data, root, false);
+            //add(data, root)            
+        }
+    }
+
+    private void add(int data, BinaryNode currentNode, boolean found) {
+        if (found) {
+            if (fatherposition) {
+                currentNode.setRight(new BinaryNode(data));
+            } else {
+                currentNode.setLeft(new BinaryNode(data));
+            }
+        }else if (data < currentNode.getData()) {
+            if (currentNode.getLeft() == null) {
+                fatherposition=false;
+                add(data, currentNode, true);
+            } else {
+                add(data, currentNode.getLeft(), false);
+            }
+        } else {
+            if (currentNode.getRight()== null) {
+                fatherposition=true;
+                add(data, currentNode, true);
+            } else {
+                add(data, currentNode.getRight(), false);
+            }        
+        }
+    }
+    /**
+     * Otra forma de agregar datos sin parámetro found
+     * @param data
+     * @param currentRoot 
+     */
+    private void Add(int data, BinaryNode currentRoot) {
+        if (data < currentRoot.getData()) {
+            if (currentRoot.getLeft() == null){
+                currentRoot.setLeft(new BinaryNode(data));
+            }else{
+                Add(data, currentRoot.getLeft());
+            }
+        }else if (currentRoot.getRight() == null) {
+            currentRoot.setRight(new BinaryNode(data));
+        } else {
+            Add(data, currentRoot.getRight());
+        }
+    }
+
 }
--- Trees/src/trees/Trees.java
+++ Trees/src/trees/Trees.java
@@ -16,11 +16,19 @@ public class Trees {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        BinarySearchTree navidad = new BinarySearchTree(1);
-        navidad.root.setLeft(new BinaryNode((2)));
-        navidad.root.getLeft().setLeft(new BinaryNode((4)));
-        navidad.root.setRight(new BinaryNode(3));
-        
+        BinarySearchTree navidad = new BinarySearchTree();
+        navidad.add(1);
+        navidad.add(2);
+        navidad.add(3);
+        navidad.add(5);
+        navidad.add(4);
+        System.out.println("");
+//        navidad.root.setLeft(new BinaryNode((2)));
+//        navidad.root.getLeft().setLeft(new BinaryNode((4)));
+//        navidad.root.setRight(new BinaryNode(3));
+        System.out.println("búsqueda de dato:");
+        String resultado = (navidad.search(4)!=null)?"Dato encontrado!":"No se encuentra el dato";
+        System.out.println(resultado);
     }
     
 }
